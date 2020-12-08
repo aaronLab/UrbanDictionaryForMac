e9fb1e64fcd4710f098dd01108b37a68718777e8
Aaron Lee <aaronlab.net@gmail.com>
2020-12-08 22:28:41 +0900

    Initial Commit

---
+++ UrbanDictionaryForMac/UrbanDictionaryForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanDictionaryForMacApp.swift
+//  UrbanDictionaryForMac
+//
+//  Created by Aaron Lee on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct UrbanDictionaryForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
